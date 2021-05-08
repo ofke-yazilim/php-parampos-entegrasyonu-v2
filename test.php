8f4e7dfc688fa72de9e773fca278aa5948879a14
okesmez <omer.faruk.kesmez@gmail.com>
2021-05-08 15:42:29 +0300

    Test işlemlerinin yürütüldüğü test.php içerisinde taksit seçmeden ödemeye devam edilmesinin önüne geçildi. fix-2

--- test.php
+++ test.php
@@ -272,5 +272,14 @@ $(document).ready(function(){
 } );
 });
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-52346402-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-52346402-1');
+</script>
 </body>
 </html>
